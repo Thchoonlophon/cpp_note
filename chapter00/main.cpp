f91348bb987a2dcf472cb5902cbcb51d00a30333
unknown <chenjin.qian@xquant.com>
2020-03-17 09:14:27 +0800

    add the chapter00

---
+++ chapter00/main.cpp
@@ -0,0 +1,33 @@
+//#include <iostream>
+
+//int main() {
+//    std::cout << "Hello, World!" << std::endl;
+//    3 + 4;
+//    std::cout << "This (\") is a quote, and this (\\) is a backslash." << std::endl;
+//        std::cout << "a\tb" << std::endl;
+// This is a comment that extends over several lines
+// by using // at the beginning of each line instead of using /*
+// or */ to delimit comments.
+//    std::cout << "Does this work?" << std::endl;
+//    return 0;
+//}
+
+#include <iostream>
+
+int
+main
+(
+
+        )
+{
+    std
+    ::
+    cout
+    <<
+    "Hello, World"
+    <<
+    std
+    ::
+    endl
+    ;
+}

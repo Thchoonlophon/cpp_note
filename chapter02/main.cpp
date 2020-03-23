e87f584220300610b39559d87aa371262908bb0d
thchoonlophon <qianchenjin192518@gmail.com>
2020-03-23 16:13:50 +0800

    the first program

--- chapter02/main.cpp
+++ chapter02/main.cpp
@@ -1,16 +1,22 @@
 #include <iostream>
 #include <string>
 
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+
 int main() {
-    std::cout << "Please enter your name: ";
-    std::string name;
-    std::cin >> name;
-    const std::string greeting = "Hello, " + name + " !";
+    cout << "Please enter your name: ";
+    string name;
+    cin >> name;
+    const string greeting = "Hello, " + name + " !";
     const int pad = 1;
     const int rows = pad * 2 + 3;
-    const std::string::size_type cols = greeting.size() + pad * 2 + 2;
-    std::cout << std::endl;
-    int r = 0;
+    const string::size_type cols = greeting.size() + pad * 2 + 2;
+    cout << endl;
+/*
+//    int r = 0;
     // these are my answer,but it's too low.
 //    while (r != rows) {
 //        int c = 0;
@@ -31,23 +37,42 @@ int main() {
 //        ++r;
 //        std::cout << std::endl;
 //    }
-    while (r != rows) {
-        int c = 0;
+//    while (r != rows) {
+//        int c = 0;
+//        while (c != cols) {
+//            if (r == 0 || r == rows - 1 || c == 0 || c == cols - 1) {
+//                std::cout << "*";
+//            } else {
+//                if (r == pad + 1 && c == pad + 1) {
+//                    std::cout << greeting;
+//                    c += greeting.size() - 1;
+//                } else {
+//                    std::cout << " ";
+//                }
+//            }
+//            ++c;
+//        }
+//        ++r;
+//        std::cout << std::endl;
+//    }
+*/
+    for (int r = 0; r != rows; ++r) {
+        string::size_type c = 0;
         while (c != cols) {
-            if (r == 0 || r == rows - 1 || c == 0 || c == cols - 1) {
-                std::cout << "*";
+            if (r == pad + 1 && c == pad + 1) {
+                cout << greeting;
+                c += greeting.size();
             } else {
-                if (r == pad + 1 && c == pad + 1) {
-                    std::cout << greeting;
-                    c += greeting.size() - 1;
+                if (r == 0 || r == rows - 1 ||
+                    c == 0 || c == cols - 1) {
+                    cout << "*";
                 } else {
-                    std::cout << " ";
+                    cout << " ";
                 }
+                ++c;
             }
-            ++c;
         }
-        ++r;
-        std::cout << std::endl;
+        cout << endl;
     }
     return 0;
 }

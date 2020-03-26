1e13d1828067c8fdf6616ce0dd8698240dffd7cb
thchoonlophon <qianchenjin192518@gmail.com>
2020-03-26 16:52:05 +0800

    new

--- chapter03/main.cpp
+++ chapter03/main.cpp
@@ -2,10 +2,12 @@
 #include <ios>
 #include <iomanip>
 #include <string>
+#include <vector>
 
 using std::cin;     using std::setprecision;
 using std::cout;    using std::string;
 using std::endl;    using std::streamsize;
+using std::vector;
 
 int main() {
     cout << "Please input your name:";
@@ -16,13 +18,17 @@ int main() {
     double midterm, final;
     cin >> midterm >> final;
     cout << "Please input your homework grades:";
-    int count = 0;
-    double sum = 0;
+//    int count = 0;
+//    double sum = 0;
     double x;
+    vector<double> homework;
     while (cin >> x) {
-        ++count;
-        sum += x;
+//        ++count;
+//        sum += x;
+        homework.push_back(x);
     }
+    typedef vector<double>::size_type vec_sz;
+    vec_sz size = homework.size();
     streamsize prec = cout.precision();
     cout << "Your final grade is " << setprecision(3)
          << midterm * 0.2 + final * 0.4 + sum / count * 0.4

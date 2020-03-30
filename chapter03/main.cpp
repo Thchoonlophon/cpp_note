06b52b9a667baf635476ac9f71d8b91e06f34016
thchoonlophon <qianchenjin192518@gmail.com>
2020-03-30 13:56:32 +0800

    entire program

--- chapter03/main.cpp
+++ chapter03/main.cpp
@@ -1,13 +1,14 @@
-#include <iostream>
+#include <algorithm>
 #include <iomanip>
+#include <ios>  // have no idea with it,the program can work without this include
+#include <iostream>
 #include <string>
 #include <vector>
-#include <algorithm>
 
-using std::cin;     using std::setprecision;
-using std::cout;    using std::string;
-using std::endl;    using std::streamsize;
-using std::vector;  using std::sort;
+using std::cin;             using std::sort;
+using std::cout;            using std::streamsize;
+using std::endl;            using std::string;
+using std::setprecision;    using std::vector;
 
 int main() {
     cout << "Please input your name:";
@@ -18,26 +19,24 @@ int main() {
     double midterm, final;
     cin >> midterm >> final;
     cout << "Please input your homework grades:";
-    double x;
     vector<double> homework;
-    while (cin >> x) {
-        homework.push_back(x);
-    }
+    double x;
+    while (cin >> x) homework.push_back(x); // What??
     typedef vector<double>::size_type vec_sz;
     vec_sz size = homework.size();
     if (size == 0) {
         cout << endl << "You must enter your grades. Please try again" << endl;
         return 1;
-    } else {
-        sort(homework.begin(), homework.end());
-        vec_sz mid = size / 2;
-        double median;
-        median = size % 2 == 0 ? (homework[mid - 1] + homework[mid]) / 2 : homework[mid];
-        streamsize prec = cout.precision();
-        cout << "Your final grade is " << setprecision(3)
-             << midterm * 0.2 + final * 0.4 + median * 0.4
-             << setprecision(prec) << endl;
     }
+    sort(homework.begin(), homework.end());
+    vec_sz mid = size / 2;
+    double median;
+    median = size % 2 == 0 ? (homework[mid - 1] + homework[mid]) / 2 : homework[mid];
+    streamsize prec = cout.precision();
+    cout << "Your final grade is " << setprecision(3)
+         << midterm * 0.2 + final * 0.4 + median * 0.4
+         << setprecision(prec) << endl;
+    return 0;
 //    streamsize prec = cout.precision(3);
 //    cout << prec << endl;
 //    cout << "Your final grade is " << midterm * 0.2 + final * 0.4 + sum / count * 0.4 << endl;

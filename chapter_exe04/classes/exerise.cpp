62123eb55aba6e65ef39a5bd6bc4956a12840b1b
thchoonlophon <qianchenjin192518@gmail.com>
2020-08-20 16:44:24 +0800

    add exerise head files

---
+++ chapter_exe04/classes/exerise.cpp
@@ -0,0 +1,21 @@
+//
+// Created by chenjin.qian on 2020/8/20.
+//
+
+#include "exerise.h"
+#include <string>
+
+using std::to_string;
+
+vector<unsigned int> max_length(double num) {
+    unsigned int maxlen1 = 0, maxlen2 = 0;
+    vector<unsigned int> out;
+    for (int i = 1; i < num; ++i) {
+        out.clear();
+        maxlen1 = max(maxlen1, to_string(i).size());
+        out.push_back(maxlen1);
+        maxlen2 = max(maxlen2, to_string(i * i).size());
+        out.push_back(maxlen2);
+    }
+    return out;
+}
---
+++ chapter_exe04/classes/exerise.h
@@ -0,0 +1,30 @@
+//
+// Created by chenjin.qian on 2020/8/20.
+//
+
+#ifndef CHAPTER_EXE04_EXERISE_H
+#define CHAPTER_EXE04_EXERISE_H
+
+
+/*#include <vector>
+#include <string>
+#include <istream>
+
+using std::vector;  using std::string;  using std::istream;
+
+struct Students_info {
+    string name;
+    double midterm, final;
+    vector<double> homework;
+};
+
+istream &read(istream &, Students_info &);*/
+#include <algorithm>
+#include <vector>
+
+using std::max;     using std::vector;
+
+vector<unsigned int> max_length(double);
+
+
+#endif //CHAPTER_EXE04_EXERISE_H

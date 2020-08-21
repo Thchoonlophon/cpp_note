c1482c78ea22bb152d5dc659276fa14e991e5c4c
thchoonlophon <qianchenjin192518@gmail.com>
2020-08-21 16:49:14 +0800

    Update students.cpp
    the source file for students

--- chapter05/source/students.cpp
+++ chapter05/source/students.cpp
@@ -2,4 +2,10 @@
 // Created by chenjin.qian on 2020/8/21.
 //
 
-#include "../head/students.h"
+#include "../head/get_grades.h"
+
+
+bool fgrade(const Student_info &s) {
+    bool out = grade(s) < 60;
+    return out;
+}

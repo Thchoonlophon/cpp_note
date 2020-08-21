ad6a67b99ad8b557f852c856471011b4921c0c52
thchoonlophon <qianchenjin192518@gmail.com>
2020-08-21 16:47:18 +0800

    Create median.h
    Created head file to gey median

---
+++ chapter05/head/median.h
@@ -0,0 +1,15 @@
+//
+// Created by chenjin.qian on 2020/8/21.
+//
+
+#ifndef CHAPTER05_MEDIAN_H
+#define CHAPTER05_MEDIAN_H
+
+
+#include <algorithm>
+#include <vector>
+
+double get_median(const std::vector<double> &);
+
+
+#endif //CHAPTER05_MEDIAN_H

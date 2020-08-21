9c327e2f22b220ce6eabee0c373429df1431c7af
thchoonlophon <qianchenjin192518@gmail.com>
2020-08-21 16:48:52 +0800

    Create median.cpp
    The source file for median

---
+++ chapter05/source/median.cpp
@@ -0,0 +1,17 @@
+//
+// Created by chenjin.qian on 2020/8/21.
+//
+
+#include "../head/median.h"
+
+using std::vector;  using std::sort;
+
+double get_median(const vector<double> &hw) {
+    vector<double>::size_type size;
+    vector<double>::size_type mid;
+    size = hw.size();
+    mid = size / 2;
+    sort(hw.begin(), hw.end());
+    double out = size % 2 == 0 ? (hw[mid] + hw[mid - 1]) / 2 : hw[mid];
+    return out;
+}

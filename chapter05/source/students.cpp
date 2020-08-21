0ce1bba7975f299bc3adcc7aea33c726d157e676
thchoonlophon <qianchenjin192518@gmail.com>
2020-08-21 17:27:51 +0800

    Update students.cpp

--- chapter05/source/students.cpp
+++ chapter05/source/students.cpp
@@ -3,9 +3,32 @@
 //
 
 #include "../head/get_grades.h"
+#include <iostream>
 
+using std::istream;     using std::vector;
+using std::cin;
 
 bool fgrade(const Student_info &s) {
     bool out = grade(s) < 60;
     return out;
+}
+
+bool compare(const Student_info &x, const Student_info &y) {
+    bool out = x.name < y.name;
+    return out;
+}
+
+istream &read_hw(istream &in, vector<double> &hw) {
+    if (in) {
+        hw.clear();
+        double x;
+        while (cin>>x)
+            hw.push_back(x);
+        in.clear();
+    }
+    return in;
+}
+
+istream &read(istream &in, Student_info &stu) {
+
 }

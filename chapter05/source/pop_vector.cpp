86e958b870e45ceace4be2b983f92dd1c952d98d
thchoonlophon <qianchenjin192518@gmail.com>
2020-08-25 16:26:22 +0800

    Update pop_vector.cpp
    finished the all source files

--- chapter05/source/pop_vector.cpp
+++ chapter05/source/pop_vector.cpp
@@ -2,3 +2,19 @@
 // Created by chenjin.qian on 2020/8/25.
 //
 
+#include "../head/pop_vector.h"
+
+using std::vector;
+
+vector<Student_info> extract_fails(vector<Student_info> &students) {
+    vector<Student_info> fail;
+    vector<Student_info>::size_type i = 0;
+    while (i != students.size()) {
+        if (fgrade(students[i])) {
+            fail.push_back(students[i]);
+            students.erase(students.begin() + i);
+        } else
+            ++i;
+    }
+    return fail;
+}

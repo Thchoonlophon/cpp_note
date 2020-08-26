cfe0e1dd160ffa6571352313ab599a64f1e06015
thchoonlophon <qianchenjin192518@gmail.com>
2020-08-26 16:45:57 +0800

    Update main.cpp
    finished the main file

--- chapter05_2/main.cpp
+++ chapter05_2/main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+#include "header/strings.h"
+
+using std::vector;  using std::string;
+using std::cout;    using std::getline;
+using std::cin;     using std::endl;
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    string s;
+    while (getline(cin, s)) {
+        vector<string> v = split(s);
+        for (vector<string>::size_type i = 0; i != v.size(); ++i) {
+            cout << v[i];
+        }
+    }
     return 0;
 }

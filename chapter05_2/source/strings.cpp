e447e13f66030825e6fbec4e5e6547130727b54a
thchoonlophon <qianchenjin192518@gmail.com>
2020-08-26 16:39:58 +0800

    Update strings.cpp
    finished the source code

--- chapter05_2/source/strings.cpp
+++ chapter05_2/source/strings.cpp
@@ -3,3 +3,25 @@
 // Created by chenjin.qian on 2020/8/26.
 //
 
+#include "../header/strings.h"
+
+using std::vector;  using std::string;
+using std::isspace;
+
+vector<string> split(const string& s){
+    typedef string::size_type str_size;
+    str_size i=0;
+    vector<string> ret;
+    while (i!=s.size()){
+        while (i!=s.size()&&isspace(s[i]))
+            ++i;
+        str_size j = i;
+        while (j!=s.size()&&!isspace(s[j]))
+            ++j;
+        if (i!=j){
+            ret.push_back(s.substr(i,j-i));
+            i=j;
+        }
+    }
+    return ret;
+}

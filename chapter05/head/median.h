37f6d0a96ce7da7ab094ed2e770c94cb1d73e98e
thchoonlophon <qianchenjin192518@gmail.com>
2020-08-26 15:31:19 +0800

    Update median.h
    commented all code to keep the other

--- chapter05/head/median.h
+++ chapter05/head/median.h
@@ -1,3 +1,4 @@
+/*
 //
 // Created by chenjin.qian on 2020/8/21.
 //
@@ -9,7 +10,8 @@
 #include <algorithm>
 #include <vector>
 
-double get_median(const std::vector<double> &);
+//double get_median(const std::vector<double> &);
 
 
 #endif //CHAPTER05_MEDIAN_H
+*/

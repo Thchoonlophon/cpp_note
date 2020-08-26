2ea9fbd3dd89435790d31e29fe0297aed54e3637
thchoonlophon <qianchenjin192518@gmail.com>
2020-08-26 17:09:03 +0800

    Update strings.cpp
    finished the source code

--- chapter05_2/source/strings.cpp
+++ chapter05_2/source/strings.cpp
@@ -6,22 +6,29 @@
 #include "../header/strings.h"
 
 using std::vector;  using std::string;
-using std::isspace;
+using std::isspace; using std::max;
 
-vector<string> split(const string& s){
+vector<string> split(const string &s) {
     typedef string::size_type str_size;
-    str_size i=0;
+    str_size i = 0;
     vector<string> ret;
-    while (i!=s.size()){
-        while (i!=s.size()&&isspace(s[i]))
+    while (i != s.size()) {
+        while (i != s.size() && isspace(s[i]))
             ++i;
         str_size j = i;
-        while (j!=s.size()&&!isspace(s[j]))
+        while (j != s.size() && !isspace(s[j]))
             ++j;
-        if (i!=j){
-            ret.push_back(s.substr(i,j-i));
-            i=j;
+        if (i != j) {
+            ret.push_back(s.substr(i, j - i));
+            i = j;
         }
     }
     return ret;
+}
+
+string::size_type width(const vector<string> &v) {
+    string::size_type maxlen = 0;
+    for (vector<string>::size_type i = 0; i != v.size(); ++i)
+        maxlen = max(maxlen, v[i].size());
+    return maxlen;
 }

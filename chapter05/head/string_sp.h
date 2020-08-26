6b37ceb87a04bb554d2b8928fd2d56720972e17b
thchoonlophon <qianchenjin192518@gmail.com>
2020-08-26 15:10:00 +0800

    Update string_sp.h
    register the function to header file

--- chapter05/head/string_sp.h
+++ chapter05/head/string_sp.h
@@ -5,4 +5,9 @@
 #ifndef CHAPTER05_STRING_SP_H
 #define CHAPTER05_STRING_SP_H
 
+#include <vector>
+#include <string>
+
+std::vector<std::string> split(const std::string&);
+
 #endif //CHAPTER05_STRING_SP_H

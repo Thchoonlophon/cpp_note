4291a4b9089c2a3d3c609ff8a6f155c48856025b
thchoonlophon <qianchenjin192518@gmail.com>
2020-08-26 14:41:06 +0800

    Update pop_vector.h
    modified the header file use the list type

--- chapter05/head/pop_vector.h
+++ chapter05/head/pop_vector.h
@@ -6,9 +6,10 @@
 #define CHAPTER05_POP_VECTOR_H
 
 #include "students.h"
+#include <list>
 
 std::vector<Student_info> extract_fails(std::vector<Student_info>&);
 
-std::vector<Student_info> extract_fail(std::vector<Student_info>&);
+std::list<Student_info> extract_fail(std::list<Student_info>&);
 
 #endif //CHAPTER05_POP_VECTOR_H

c3cf083e553b046a681f6e4212c426a6fb8cd969
thchoonlophon <qianchenjin192518@gmail.com>
2020-08-26 15:33:54 +0800

    Update main.cpp
    modified the main file to run another program

--- chapter05/source/main.cpp
+++ chapter05/source/main.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
-#include <iomanip>
-#include "../head/get_grades.h"
+//#include <iomanip>
+//#include "../head/get_grades.h"
+#include "../head/string_sp.h"
 
 using std::vector;     using std::streamsize;
-using std::setw;       using std::setprecision;
+//using std::setw;       using std::setprecision;
 using std::cin;        using std::cout;
 using std::endl;       using std::max;
 using std::string;     using std::domain_error;
-using std::sort;
+//using std::sort;
 
 int main() {
-    vector<Student_info> students;
+    /*vector<Student_info> students;
     Student_info record;
     string::size_type maxlen = 0;
     while (read(cin, record)) {
@@ -28,6 +29,11 @@ int main() {
             cout << e.what();
         }
         cout << endl;
+    }*/
+    string a = "   jingxun   ";
+    vector<string> b = split(a);
+    for (vector<string>::size_type i = 0; i != b.size(); ++i) {
+        cout<<b[i];
     }
     return 0;
 }

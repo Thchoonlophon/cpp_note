36f381180f43c1e61c12a0ce0ca92cec322439d0
thchoonlophon <qianchenjin192518@gmail.com>
2020-08-26 16:30:59 +0800

    Update strings.h

--- chapter05_2/header/strings.h
+++ chapter05_2/header/strings.h
@@ -5,4 +5,10 @@
 #ifndef CHAPTER05_2_STRINGS_H
 #define CHAPTER05_2_STRINGS_H
 
+#include <vector>
+#include <string>
+#include <cctype>
+
+std::vector<std::string> split(const std::string&);
+
 #endif //CHAPTER05_2_STRINGS_H

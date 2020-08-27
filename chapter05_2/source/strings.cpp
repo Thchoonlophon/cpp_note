0bcc9c12037a9d792a391bfdf896d8af86161918
thchoonlophon <qianchenjin192518@gmail.com>
2020-08-27 13:11:04 +0800

    Update strings.cpp
    get the right answer

--- chapter05_2/source/strings.cpp
+++ chapter05_2/source/strings.cpp
@@ -39,7 +39,7 @@ vector<string> frame(const vector<string> &v) {
     string border(maxlen + 4, '*');
     ret.push_back(border);
     for (vector<string>::size_type i = 0; i != v.size(); ++i) {
-        ret.push_back("*" + v[i] + string(maxlen - v[i].size(), ' ') + "*");
+        ret.push_back("* " + v[i] + string(maxlen - v[i].size(), ' ') + " *");
     }
     ret.push_back(border);
     return ret;

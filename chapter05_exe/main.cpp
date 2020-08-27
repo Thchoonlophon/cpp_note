1ea59f324db9b5553f7dd33b9479d8217e1345f2
thchoonlophon <qianchenjin192518@gmail.com>
2020-08-27 16:46:15 +0800

    Update main.cpp

--- chapter05_exe/main.cpp
+++ chapter05_exe/main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+#include <string>
+#include "header/exerises.h"
+
+using std::string;  using std::vector;
+using std::cout;    using std::endl;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    string a = "123,456,789,1011";
+    char f = ',';
+    vector<string> v = split(a, f);
+    string out = "[";
+    for (vector<string>::size_type i = 0; i != v.size(); ++i) {
+        out += "\"";
+        out += v[i];
+        out += "\"";
+        if (i < v.size() - 1)
+            out += ", ";
+    }
+    out += "]";
+    cout << out << endl;
 }

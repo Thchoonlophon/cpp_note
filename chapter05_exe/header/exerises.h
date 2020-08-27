bc04492c302ffdf35664fec55727aa9ff01d44fa
thchoonlophon <qianchenjin192518@gmail.com>
2020-08-27 14:55:31 +0800

    Update exerises.h
    added a function to header file

--- chapter05_exe/header/exerises.h
+++ chapter05_exe/header/exerises.h
@@ -5,4 +5,9 @@
 #ifndef CHAPTER05_EXE_EXERISES_H
 #define CHAPTER05_EXE_EXERISES_H
 
+#include <vector>
+#include <string>
+
+std::vector<std::string> split(std::string&, std::string&);
+
 #endif //CHAPTER05_EXE_EXERISES_H

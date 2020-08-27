206ab6def5a3b82b40c5cde61d9ff52ab7208ab3
thchoonlophon <qianchenjin192518@gmail.com>
2020-08-27 13:43:05 +0800

    Update strings.cpp
    finished the source code

--- chapter05_2/source/strings.cpp
+++ chapter05_2/source/strings.cpp
@@ -51,3 +51,19 @@ vector<string> vect(const vector<string> &top, const vector<string> &bottom) {
         ret.push_back(*it);
     return ret;
 }
+
+vector<string> hcat(const vector<string> &left, const vector<string> &right) {
+    vector<string> ret;
+    string::size_type width1 = width(left) + 1;
+    vector<string>::size_type i = 0, j = 0;
+    while (i != left.size() || j != right.size()) {
+        string s;
+        if (i != left.size())
+            s = left[i++];
+        s += string(width1 - s.size(), ' ');
+        if (j != right.size())
+            s += right[j++];
+        ret.push_back(s);
+    }
+    return ret;
+}

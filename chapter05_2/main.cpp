2bd197b55127ef7305c78a09af8705a4f0c553be
thchoonlophon <qianchenjin192518@gmail.com>
2020-08-27 09:03:08 +0800

    Update main.cpp
    test the function

--- chapter05_2/main.cpp
+++ chapter05_2/main.cpp
@@ -7,12 +7,23 @@ using std::cin;     using std::endl;
 
 
 int main() {
-    string s;
+    /*string s;
     while (getline(cin, s)) {
         vector<string> v = split(s);
         for (vector<string>::size_type i = 0; i != v.size(); ++i) {
             cout << v[i];
         }
+    }*/
+    string s;
+    vector<string> v;
+    unsigned int t;
+    cin >> t;
+    cout << "**************************" << endl;
+    while (getline(cin, s) && t != 0) {
+        v.push_back(s);
+        --t;
     }
+    string::size_type maxlen = width(v);
+    cout << maxlen << endl;
     return 0;
 }

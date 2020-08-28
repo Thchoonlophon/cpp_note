c22eb3aa57d3700a9a377bc70f6b9569557bfe0d
thchoonlophon <qianchenjin192518@gmail.com>
2020-08-28 14:21:17 +0800

    Update main.cpp
    test function

--- chapter05_exe/main.cpp
+++ chapter05_exe/main.cpp
@@ -7,15 +7,16 @@ using std::cout;    using std::endl;
 
 int main() {
     string a = "123,,456,,789,,1011";
-//    char f = ',';
-    string f = ",,";
-    vector<string> v = split(a, f);
-    string out = "[";
-    for (vector<string>::size_type i = 0; i != v.size(); ++i) {
-        out += "\"" + v[i] + "\"";
-        if (i < v.size() - 1)
-            out += ", ";
-    }
-    out += "]";
-    cout << out << endl;
+    cout << replace(a, ",,", "  ") << endl;
+//    string f = ",,";
+//    vector<string> v = split(a, f);
+//    string out = "[";
+//    for (vector<string>::size_type i = 0; i != v.size(); ++i) {
+//        out += "\"" + v[i] + "\"";
+//        if (i < v.size() - 1)
+//            out += ", ";
+//    }
+//    out += "]";
+//    cout << out << endl;
+    return 0;
 }
